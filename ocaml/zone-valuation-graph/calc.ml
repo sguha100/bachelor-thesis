182136c7fc8584a1f7b9965b3415a18adb10ab8d
Mihir Mehta <mihirthegenius@gmail.com>
2013-01-22 13:04:09 +0530

    Replaced "state" with "location" everywhere.

--- ocaml/zone-valuation-graph/calc.ml
+++ ocaml/zone-valuation-graph/calc.ml
@@ -2,7 +2,7 @@
 let _ =
   let lexbuf = Lexing.from_channel stdin in
   (let result = Parser.main Lexer.token lexbuf in
-   print_int (result.Grammar_types.numstates); 
+   print_int (result.Grammar_types.numlocations); 
    print_newline(); 
    print_int (result.Grammar_types.numtrans); 
    print_newline(); 
@@ -12,7 +12,7 @@ let _ =
    print_newline(); 
    print_int (Array.length result.Grammar_types.clocks); 
    print_newline(); 
-   print_int (Array.length result.Grammar_types.states); 
+   print_int (Array.length result.Grammar_types.locations); 
    print_newline(); 
    flush stdout;
    exit 0)
