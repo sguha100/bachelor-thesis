5ddf7110d8c849918408e5f29b4da46b42d8b7d7
Mihir Mehta <mihirthegenius@gmail.com>
2013-02-10 19:05:33 +0530

    Maybe dump this stupid interface?

--- ocaml/next_step/clock_utilities.mli
+++
@@ -1,7 +0,0 @@
-open Grammar_types
-  
-val zero_valuation: timed_automaton -> float Valuation.t
-
-val add_delay: float Valuation.t -> float -> float Valuation.t
-
-val reset_clocks: float Valuation.t -> string list -> float Valuation.t
