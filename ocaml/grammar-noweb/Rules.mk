fddfce94adf2256374a66d9e8524e5483acfbd1e
Mihir Mehta <mihirthegenius@gmail.com>
2013-02-11 23:10:15 +0530

    About to start editing the interface of the timed automaton.

--- ocaml/grammar-noweb/Rules.mk
+++ ocaml/grammar-noweb/Rules.mk
@@ -5,7 +5,6 @@ d		:= $(dir)
 TGT_$(d) := $(d)/calc.ml $(d)/lexer.mll $(d)/parser.mly \
 $(d)/grammar_types.ml $(d)/grammar_types.mli $(d)/calc.native
 
-# all: calc.native
 $(TGT_$(d)): d := $(d)
 #This is a target-specific variable, meant to
 #prevent the current value of d from getting lost when a target from
@@ -17,11 +16,11 @@ $(d)/lexer.mll:$(d)/lexer.mll.nw
 	notangle -Rlexer.mll $(d)/lexer.mll.nw >$(d)/lexer.mll
 $(d)/parser.mly:$(d)/parser.mly.nw
 	notangle -Rparser.mly $(d)/parser.mly.nw >$(d)/parser.mly
-$(d)/grammar_types.ml:$(d)/grammar_types.ml.nw
-	notangle -Rgrammar_types.ml $(d)/grammar_types.ml.nw \
+$(d)/grammar_types.ml:$(d)/grammar_types.nw
+	notangle -Rgrammar_types.ml $(d)/grammar_types.nw \
 >$(d)/grammar_types.ml
-$(d)/grammar_types.mli: $(d)/grammar_types.mli.nw
-	notangle -Rgrammar_types.mli $(d)/grammar_types.mli.nw \
+$(d)/grammar_types.mli: $(d)/grammar_types.nw
+	notangle -Rgrammar_types.mli $(d)/grammar_types.nw \
 	>$(d)/grammar_types.mli
 $(d)/calc.native:$(d)/calc.ml $(d)/lexer.mll $(d)/parser.mly \
 	$(d)/grammar_types.ml $(d)/grammar_types.mli
