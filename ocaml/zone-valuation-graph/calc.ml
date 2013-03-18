48440feb18eb6e11bcdf9d8987b6d337e3da6bf0
Mihir Mehta <mihirthegenius@gmail.com>
2013-03-18 09:58:31 +0530

    Got some kind of pretty print. Next up: edges, then pretty print edges, then convert things to a decent format, then apply fernandez' algorithm.

--- ocaml/zone-valuation-graph/calc.ml
+++ ocaml/zone-valuation-graph/calc.ml
@@ -13,20 +13,45 @@ let _ =
   let
       unit = Printf.printf "Zone-valuation graph generated successfully!\n"
   in
-  Printf.printf "numlocations = %s\n" (string_of_int result.numlocations);
-  Printf.printf "numtrans = %s\n" (string_of_int result.numtrans);
-  Printf.printf "numclocks = %s\n" (string_of_int result.numclocks);
-  Printf.printf "numinit = %s\n" (string_of_int result.numinit);
+  Printf.printf "#locations %s\n" (string_of_int result.numlocations);
+  Printf.printf "#trans %s\n" (string_of_int result.numtrans);
+  Printf.printf "#clocks %s\n" (string_of_int result.numclocks);
+  Printf.printf "#actions %s\n" (string_of_int result.numactions);
+  Printf.printf "#init %s\n" (string_of_int result.numinit);
   let len = (Array.length g) in
   for i = 0 to len - 1 do
-    Printf.printf
-      "i = %s, %s\n"
-      (string_of_int
-         i)
-      (string_of_int
-         (List.length
-            g.(i)
-         ));
+    (* Printf.printf *)
+    (*   "i = %s, %s\n" *)
+    (*   (string_of_int *)
+    (*      i) *)
+    (*   (string_of_int *)
+    (*      (List.length *)
+    (*         g.(i) *)
+    (*      )); *)
+    List.iter
+      (function zone ->
+        Printf.printf "\nlocation: %s\n" (string_of_int i);
+        Printf.printf
+          "invar: %s\n"
+          (String.concat
+             " AND "
+             (List.map
+                (function
+                | True -> "TRUE"
+                | False -> "FALSE"
+                | Lt (cn, n) -> cn ^ " < " ^ (string_of_int n)
+                | Le (cn, n) -> cn ^ " <= " ^ (string_of_int n)
+                | Eq (cn, n) -> cn ^ " = " ^ (string_of_int n)
+                | Ge (cn, n) -> cn ^ " >= " ^ (string_of_int n)
+                | Gt (cn, n) -> cn ^ " > " ^ (string_of_int n)
+                )
+                zone.zone_constraint
+             )
+          )
+        ;
+      )
+      g.(i)
+    ;
   done;
   flush stdout;
   exit 0
