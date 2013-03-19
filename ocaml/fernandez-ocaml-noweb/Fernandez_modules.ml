2ccd9cd65b1cdb91e389dc2b32ea5e2ee9c8d291
Mihir Mehta <mihirthegenius@gmail.com>
2013-03-20 03:40:04 +0530

    Added a function to generate a DOT file from a generic LTS.

--- ocaml/fernandez-ocaml-noweb/Fernandez_modules.ml
+++ ocaml/fernandez-ocaml-noweb/Fernandez_modules.ml
@@ -3,8 +3,8 @@ sig
   type node_ref_t
   type action_t
   type lts_t
-  val expand_node: node_ref_t -> string list
-  val expand_action: action_t -> string
+  val node_name: lts_t -> node_ref_t -> string
+  val expand_action: lts_t -> action_t -> string
   val nodes: lts_t -> node_ref_t list
   val actions: lts_t -> action_t list
   val in_adjacency: lts_t -> node_ref_t -> (action_t * (node_ref_t list)) list
@@ -17,7 +17,7 @@ struct
   type action_t = LT.action_t
       (* type lts = {nodes: node_ref_t array; actions: action_t
   array} *)
-  let expand_node = LT.expand_node
+  let node_name = LT.node_name
   let expand_action = LT.expand_action
   let nodes = LT.nodes
   let actions = LT.actions
@@ -251,8 +251,54 @@ let fernandez l =
   let
       llistblock = (get_block_from_node_refs l (nodes l))
   in
-  emptyqueue l ([Simple llistblock], [llistblock])
+  let
+      (_, pi) = emptyqueue l ([Simple llistblock], [llistblock])
+  in
+  pi
 
+let print_dot l partition filename =
+  let
+      out = open_out filename  
+  in
+  Printf.fprintf out "digraph \"Bisimilarity quotient graph\" {\n";
+  List.fold_left
+    (function count -> function block ->
+      Printf.fprintf out
+        "subgraph cluster_%s { \nrank = same; %s ;\n}\n"
+        (string_of_int count)
+        (String.concat
+           ";"
+           (List.map
+              (function node_ref -> "\"" ^ (node_name l node_ref) ^ "\"")
+              block.node_refs
+           )
+        )
+      ;
+      List.iter
+        (function node_ref ->
+          (* Printf.fprintf out "\"%s\";\n" (node_name l node_ref); *)
+          List.iter
+            (function (a, nl) ->
+              List.iter
+                (function nlelem ->
+                  Printf.fprintf
+                    out
+                    "\"%s\" -> \"%s\" [label = \"%s\"]\n"
+                    (node_name l node_ref)
+                    (node_name l nlelem)
+                    (expand_action l a)
+                )
+                nl
+            )
+            (in_adjacency l node_ref);
+        )
+        block.node_refs;
+      (count + 1)
+    )
+    0
+    partition;
+  Printf.fprintf out "}\n";
+  flush out
 end
 
 module SimpleLT =
@@ -262,8 +308,8 @@ module SimpleLT =
     type lts_t =
       {action_count : action_t;
        in_adjacencies: ((action_t *(node_ref_t list)) list) array}
-    let expand_node = function i -> [string_of_int i]
-    let expand_action = function a -> string_of_int a
+    let node_name l i= (string_of_int i)
+    let expand_action l a = string_of_int a
     let nodes=
       function l ->
         Array.to_list (Array.init (Array.length l.in_adjacencies) (function i -> i))
@@ -274,3 +320,21 @@ module SimpleLT =
   end
 
 module SimpleLTS = LTS (SimpleLT)
+let lts1 = {SimpleLT.in_adjacencies = [|[(2, [6])]; 
+                    [(0, [1])]; 
+                    [(0, [0]); (1, [7])];
+                    [(1, [2])];
+                    [(1, [2])];
+                    [(0, [4; 5]); (2, [6])];
+                    [(1, [3]); (2, [2])];
+                    [(0, [1])]|]; SimpleLT.action_count=3}
+let pi1 = SimpleLTS.fernandez lts1
+let lts2 = {SimpleLT.in_adjacencies=[|[(0, [5])]; 
+                    [(0, [0; 1])]; 
+                    [(1, [4])];
+                    [(0, [6]); (1, [2])];
+                    [(1, [3])];
+                    [];
+                    [(1, [5])]
+                  |]; SimpleLT.action_count=2}
+let pi2 = SimpleLTS.fernandez lts2
