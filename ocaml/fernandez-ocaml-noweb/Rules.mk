bc0e391b18f6888c1d88f59c8dc7e6fd534202a1
Mihir Mehta <mihirthegenius@gmail.com>
2013-03-26 08:36:28 +0530

    Removed a lot of redundant stuff from fernandez-ocaml-noweb

--- ocaml/fernandez-ocaml-noweb/Rules.mk
+++ ocaml/fernandez-ocaml-noweb/Rules.mk
@@ -2,9 +2,7 @@ sp := $(sp).x
 dirstack_$(sp) := $(d)
 d := $(dir)
 
-TGT_$(d) := fernandez_app.native $(d)/fernandez_app.ml \
- $(d)/fernandez.ml $(d)/fernandez.mli $(d)/fernandez.html \
-$(d)/Fernandez_modules.ml
+TGT_$(d) := $(d)/Fernandez_modules.ml
 
 $(TGT_$(d)): d := $(d)
 #This is a target-specific variable, meant to
@@ -15,27 +13,9 @@ $(TGT_$(d)): $(d)/Rules.mk
 #We do want the related targets to be re-built when the Makefile
 #fragment is updated.
 
-fernandez_app.native: $(d)/fernandez_app.ml $(d)/fernandez.ml \
-$(d)/fernandez.mli
-	ocamlbuild $(d)/fernandez_app.native
-
-$(d)/fernandez_app.ml: $(d)/fernandez.nw
-	notangle -Rfernandez_app.ml $(d)/fernandez.nw \
-	>$(d)/fernandez_app.ml
-
 $(d)/Fernandez_modules.ml: $(d)/Fernandez_modules.nw
 	notangle -RFernandez_modules.ml $(d)/Fernandez_modules.nw \
 	>$(d)/Fernandez_modules.ml
-
-$(d)/fernandez.ml:$(d)/fernandez.nw;echo $(d)
-	notangle -Rfernandez.ml $(d)/fernandez.nw >$(d)/fernandez.ml
-
-$(d)/fernandez.mli:$(d)/fernandez.nw
-	notangle -Rfernandez.mli $(d)/fernandez.nw > $(d)/fernandez.mli
-
-$(d)/fernandez.html:$(d)/fernandez.nw
-	noweave -filter l2h -index -html $(d)/fernandez.nw | htmltoc \
-	>$(d)/fernandez.html
 
 d := $(dirstack_$(sp))
 sp := $(basename $(sp))
