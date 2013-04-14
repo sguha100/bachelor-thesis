d737e879c266d136678a6a93cfeefbab2baf20a0
Mihir Mehta <mihirthegenius@gmail.com>
2013-04-15 01:59:38 +0530

    Made some tests more readable.

--- ocaml/c/Zone_stubs_test.ml
+++ ocaml/c/Zone_stubs_test.ml
@@ -35,18 +35,7 @@ let verify_raw_t_translation clock_names clock_constraint expected =
 
 let test41 =
   if
-    (match
-        (clock_constraint_to_raw_t_option [|"X"|] [Le ("X", 3)])
-     with
-     | None -> false
-     | Some dbm ->
-       match
-         dbm_toConstraintList dbm 2
-       with
-       | [(i, j, strictness, bound)] ->
-          (i = 1) && (j = 0) && not(strictness) && (bound = 3)
-       | _ -> false
-    )
+    verify_raw_t_translation [|"X"|] [Le ("X", 3)] [(1, 0, false, 3)]
   then
     "test41 passed"
   else
@@ -54,18 +43,7 @@ let test41 =
 
 let test42 =
   if
-    (match
-        (clock_constraint_to_raw_t_option [|"X"|] [Gt ("X", 3)])
-     with
-     | None -> false
-     | Some dbm ->
-       match
-         dbm_toConstraintList dbm 2
-       with
-       | [(i, j, strictness, bound)] ->
-          (i = 0) && (j = 1) && strictness && (bound = -3)
-       | _ -> false
-    )
+    verify_raw_t_translation [|"X"|] [Gt ("X", 3)] [(0, 1, true, -3)]
   then
     "test42 passed"
   else
@@ -73,19 +51,10 @@ let test42 =
 
 let test43 =
   if
-    (match
-        (clock_constraint_to_raw_t_option [|"X"; "Y"|] [Gt ("Y", 3);
-                                                        Ge ("Y", 3)])
-     with
-     | None -> false
-     | Some dbm ->
-       match
-         dbm_toConstraintList dbm 3
-       with
-       | [(i, j, strictness, bound)] ->
-          (i = 0) && (j = 2) && strictness && (bound = -3)
-       | _ -> false
-    )
+    verify_raw_t_translation
+      [|"X"; "Y"|]
+      [Gt ("Y", 3); Ge ("Y", 3)]
+      [(0, 2, true, -3)]
   then
     "test43 passed"
   else
