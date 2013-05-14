cab07677091f82e87fd4dc7cf7943972403564e9
Mihir Mehta <mihirthegenius@gmail.com>
2013-05-14 12:38:47 +0530

    Moved ZVG_tree into clutter - we aren't using it any more.

--- ocaml/clutter/Rules.mk
+++ ocaml/clutter/Rules.mk
@@ -5,7 +5,8 @@ d := $(dir)
 dir := $(d)/next_step
 include $(dir)/Rules.mk
 
-TGT_$(d) := 
+TGT_$(d) := $(d)/ZVG_tree.ml \
+$(d)/ZVG_tree.ml.tex
 
 $(TGT_$(d)): d := $(d)
 #This is a target-specific variable, meant to
@@ -15,6 +16,12 @@ $(TGT_$(d)): d := $(d)
 $(TGT_$(d)): $(d)/Rules.mk
 #We do want the related targets to be re-built when the Makefile
 #fragment is updated.
+
+$(d)/%.ml: $(d)/%.ml.nw
+	notangle -R$(subst $(d)/,,$@) $< > $@
+
+$(d)/%.ml.tex: $(d)/%.ml.nw
+	noweave -filter 'sed "/^@use /s/_/\\\\_/g;/^@defn /s/_/\\\\_/g"' -index -latex $< > $@
 
 # The argument passed with the -R switch does not require
 # qualification with $(d) because it refers solely to the file name
